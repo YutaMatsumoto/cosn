75f8d46ffd2ede82c158b3a21485c66ba0b98086
Yuta Matusmoto <utah.matsumoto@gmail.com>
2013-10-26 21:21:06 -0700

    Start implementing PROFILE message

--- client.py
+++ client.py
@@ -32,13 +32,15 @@
 #	"peer" is used as a preifx for methods that send requests to peer servers.
 #
 
+from profile import *
+import errno
+import os
 import signal
 import sys
 from socket import *
 
 # ------------------------------------------------------------------------------
 # Util
-
 def get_open_port():
 	# From [get open TCP port in Python - Stack Overflow](http://stackoverflow.com/questions/2838244/get-open-tcp-port-in-python)
 	import socket
@@ -61,7 +63,6 @@ def parse_command():
 	print "uid, server ip, server port = ", uid, cs_ip, cs_port
 	return uid, cs_ip, cs_port
 
-
 # ------------------------------------------------------------------------------
 # ChatScreen TODO
 class ChatScreen: 
@@ -72,7 +73,6 @@ class ChatScreen:
 
 # ------------------------------------------------------------------------------
 # Client
-
 class Client: 
 
 	debug = True
@@ -81,6 +81,7 @@ class Client:
 	uid = -1 # user ID of this client, registered on the central server
 	chatting = False
 	chat_screen = ChatScreen()
+	profile = None
 
 	# Peer Connection Variables
 	chat_counter = 0
@@ -94,7 +95,6 @@ class Client:
 	cs_port = -1 # port number of central server for UDP requests
 	cs_sock = socket(AF_INET, SOCK_DGRAM) # udp socket to connect to server
 
-
 	def __init__(self, uid, cs_ip, cs_port): 
 		self.chat_screen = ChatScreen()
 		self.uid = uid
@@ -103,6 +103,7 @@ class Client:
 		self.ps_sock.bind(('',self.ps_port))
 		self.ps_sock.listen(1)
 		self.udp_register()
+		self.profile = Profile("profile.xml")
 
 	def terminate(self): 
 			self.ps_sock.close()	
@@ -171,7 +172,6 @@ class Client:
 			print "From Central Server: ", message
 		return message, address
 
-
 	# ------------------------------------------------------------------------
 	# As Peer Server 
 	
@@ -208,9 +208,16 @@ class Client:
 				else: 
 					if self.debug:
 						print "Ignoring CHAT message since FRIEND message has not been received."
-			else: 
+			elif words[0] == "REQUEST": 
+				version = words[1]
+				self.peer_profile(version, sock)
+			elif words[0] == "RELAY": # TODO
+				uid = words[1]
+				version = words[2]
+				self.peer_profile(version, sock)
+			else:
 				print "Received insensible message: ", message
-			sock.close()
+			sock.close() # TODO do not close until terminate message
 
 		except timeout:
 			print "Timeout in serve_tcp_request()"
@@ -229,21 +236,19 @@ class Client:
 			print "To   Peer: ", message
 		sock.send(message)
 
-	# def _peer_init_connection(self, ip, port, timeout):
-	# 	if self.p_conn_sock_inited == False:
-	# 		self.p_conn_sock = socket(AF_INET, SOCK_STREAM)
-	# 		self.p_conn_sock.settimeout(timeout)
-	# 		self.p_conn_sock.connect((ip,int(port)))
-	# 		# TODO: Make sure it is inited according to ip and port
-	# 		self.p_conn_sock_inited = True 
-
 	def init_tcp_conn(self,ip,port): 
 		# TODO ip and port should be replaced by uid and the connection information should be cached
 		sock = socket(AF_INET, SOCK_STREAM)
 		sock.settimeout(2)           
 		sock.connect((ip,int(port)))       
 		return sock
-		
+
+	def address_of_peer(self, peer):
+		location = self.udp_query( peer ) # TODO saori to saved peer
+		words = location.split()
+		ip = words[1]
+		port = words[2]
+		return ip,port
 
 	def peer_friend(self, peer_id):
 		location = self.udp_query( peer_id )
@@ -274,24 +279,38 @@ class Client:
 		self.chat_counter += 1
 		self._send_to_peer(sock, "CHAT "+ str(self.chat_counter) + " " + message)
 		self._receive_from_peer(sock)
-			
-	def address_of_peer(self, peer):
-		location = self.udp_query( peer ) # TODO saori to saved peer
-		words = location.split()
-		ip = words[1]
-		port = words[2]
-		return ip,port
 
 	def peer_request(self, peer, profile_version):
+
+		# Send REQUEST message
 		ip,port = self.address_of_peer(peer)
 		sock = self.init_tcp_conn(ip,port)
 		self._send_to_peer(sock, "REQUEST " + str(profile_version) )
 
+		# receive profile update for new versions
+		update = self._receive_from_peer(sock)
+
+		# TODO on phase2
+		# update local peer profile on hard disk
+		# peer_profile_file = peer+"-profile.xml" 
+		# profile = Profile(peer_profile_file)
+		# profile.extend( update )
+		# print "peer_request after extend ", profile.tostring()
+		# print "TODO in peer_request : flush"
+		# profile.flush()
 
 
+	def peer_profile(self, version_in_request, sock): 
+		# note file_length is sent as an ascii string
+		# TODO indicate current version
+		# Create xml file that consists of all the profiles since the version_in_request
+		current_profile_version  = self.profile.current_version_number()
+		profile_update = self.profile.versions_between(int(version_in_request)+1, current_profile_version ).tostring()
+		profile_update_length = str( len(profile_update) )
+		message = self.uid + " " + str(current_profile_version) + " " +  str(profile_update_length) + " " + profile_update
 
+		self._send_to_peer( sock, message )
 		
-
 
 
 # ------------------------------------------------------------------------------
@@ -307,6 +326,9 @@ class Client:
 # ------------------------------------------------------------------------------
 # Main
 
+# profile = Profile( "profile.xml" )
+# print profile.tostring()
+
 def signal_handler(signal, frame):
 	print "SIGINT received"
 	client.terminate()
@@ -316,6 +338,7 @@ signal.signal(signal.SIGINT, signal_handler)
 # if __name__ = "__main__":
 uid, cs_ip, cs_port = parse_command()
 u = Client(uid,cs_ip,cs_port)
+
 # u.udp_register(uid, "127.0.0.1")
 if u.uid == "saori":
 	while 1: 
@@ -323,6 +346,7 @@ if u.uid == "saori":
 elif u.uid == "ymat" or u.uid == "yuta":
 	u.peer_friend("saori")
 	u.peer_chat("saori", "Waaatup!!!")	
-	u.peer_request("saori", 1)
+	u.peer_request("saori", 2)
+	# u.peer_request("saori", 2)
 else:
 	print "uid not yuta/ymat nor soari"
---
+++ profile.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+
+# [19.7. xml.etree.ElementTree - The ElementTree XML API - Python v2.7.5 documentation] 
+#   http://docs.python.org/2/library/xml.etree.elementtree.html#reference
+
+import os
+import xml.etree.ElementTree as ET
+
+class Profile:
+
+	profiles = None # root Element <profile>
+	profiles_file = None
+
+	def __init__(self, profile):
+		# TODO IOError
+		if profile is not None:
+			# If peer profile exists, just load it as Element
+			if os.path.exists( profile ):
+				self.profiles = ET.parse(profile).getroot()
+			# If peer profile does not exist, create it on the spot with empty content.
+			else:
+				self.profiles = ET.Element("profile")
+				self.tostring()	
+			# set up filename for the profile
+			self.profiles_file = profile
+		else:
+			# TODO : Error handling in all the other functions if this is the case
+			self.profiles = None
+
+	def __del__(self):
+		print "Profile __del__: implement flushing logic to profile." 
+
+	# TODO on phase2
+	# def extend(self, xmlstring ):
+	# 	print "Before ET.fromstring() : xmlstring is ", xmlstring
+	# 	profile = ET.fromstring( xmlstring )
+	# 	for version in profile.findall("versoin"):
+	# 		profiles.insert( len(profiles), versoin )	
+	# 	self.flush()
+
+	# TODO on phase2
+	# def flush(self):
+	# 	f = open( self.profiles_file, "wr" )
+	# 	f.write( self.tostring() )
+	
+	def get_profiles(self):
+		return self.profiles
+
+	def current_version_number(self):
+		# TODO : Could be better to assume that the current version is the most recent version
+		# TODO : Think about if the profile is not in good format. For example current_version tag is not there.
+		current_version = self.profiles.find("current_version")
+		current_version_number = current_version.text
+		return int(current_version_number)
+
+	def versions_between(self, low, high):
+		profiles = []
+		profile_history = self.get_profiles()
+		for ver in profile_history.findall("version"):
+			version_number = int( ver.attrib["id"] )
+			if version_number < low :
+				continue
+			elif version_number > high:
+				continue
+			profiles.append(ver)
+		wrap = ET.Element('profile')
+		for profile in profiles:
+			wrap.insert(len(wrap), profile)
+		p = Profile(None)
+		p.profiles = wrap
+		return p
+
+	def tostring(self):
+		return ET.tostring( self.profiles )
+
+if __name__ == "__main__":
+	p = Profile("profile.xml")
+	print "Current Version is " + str(p.current_version_number())
+	print p.versions_between(1,3).tostring()
